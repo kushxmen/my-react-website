3e4e1b6e07de7583295e7f647eb130a63e648b83
Kush Patel <patel.kush@tcs.com>
2020-09-12 12:16:45 +0530

    Updates

---
+++ precache-manifest.5faeafc11602b823cffabf3106e2f717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd51286d55274131cd54dfce870dc1b",
+    "url": "/my-react-website/index.html"
+  },
+  {
+    "revision": "bb22a5a67b6a3029e4d2",
+    "url": "/my-react-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2fd6040dd5f825a21b3",
+    "url": "/my-react-website/static/js/2.292ce158.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-react-website/static/js/2.292ce158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb22a5a67b6a3029e4d2",
+    "url": "/my-react-website/static/js/main.76f67423.chunk.js"
+  },
+  {
+    "revision": "82b89afc979ad902c36a",
+    "url": "/my-react-website/static/js/runtime-main.6ce3d8e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
